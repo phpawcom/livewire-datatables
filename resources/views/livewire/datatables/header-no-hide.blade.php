24bce7ac8fd8c32c950068bfb6f6dd652e0b2151
Abdulaziz Al Rashdi <me@alrashdi.co>
2025-03-30 06:34:22 +0400

    change in event

--- resources/views/livewire/datatables/header-no-hide.blade.php
+++ resources/views/livewire/datatables/header-no-hide.blade.php
@@ -4,7 +4,7 @@
         class="relative table-cell h-12 overflow-hidden align-top"
         @include('datatables::style-width')>
         @if($column['sortable'])
-            <button wire:click="sort('{{ $index }}')" class="w-full h-full px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider flex items-center focus:outline-none {{ $column['headerAlign'] === 'right' ? 'justify-end' : ($column['headerAlign'] === 'center' ? 'justify-center' : '') }}">
+            <button wire:click="$dispatch('sort', { index: {{ $index }}})" class="w-full h-full px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider flex items-center focus:outline-none {{ $column['headerAlign'] === 'right' ? 'justify-end' : ($column['headerAlign'] === 'center' ? 'justify-center' : '') }}">
                 <span class="inline">{{ str_replace('_', ' ', $column['label']) }}</span>
                 <span class="inline text-xs text-blue-400">
                     @if($sort === $index)
